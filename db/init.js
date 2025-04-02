b584c5d6ee3c1f33748382ae26e89e74737c9f4a
Manon <112950709+Manon-Arc@users.noreply.github.com>
2025-04-02 23:47:36 +0200

    Update init.js

--- db/init.js
+++ db/init.js
@@ -2,10 +2,4 @@ require('dotenv').config();
 
 db = db.getSiblingDB('database');
 
-db.createUser({
-  user: process.env.DB_USER,
-  pwd: process.env.DB_PWD,
-  roles: [{ role: "readWrite", db: "database" }]
-});
-
 db.test_collection.insertOne({ message: "DB initialized successfully!" });
